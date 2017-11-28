d1e6e33cb1b8be878aecc8d3c011001b7b769772
SmXme <pillot.clement@gmail.com>
2017-11-28 10:40:41 +0100

    Added the function to print the Result

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,8 @@
 // add your code here to make the calculator work
 
+//						GLOBAL VARIABlES						//
+var resultat;
+
 						//		EventListeners		//
 
 //Event when a button is clicked
@@ -11,16 +14,23 @@ $('.btn').click(addNumber);
 //Function to add the number, with some tests to neutralize operators.
 function addNumber () {
 	getNumber = $(this).html();
-	if (getNumber != "c" & getNumber != "+" & getNumber != "-" & getNumber != "*" & getNumber != "/" & getNumber != "=" ){
-		$('#result').append($(this).html());		
+	if (getNumber != "c" & getNumber != "="){
+		$('#result').append(getNumber);		
 	}
-	else {
-		($(this).html() == "c")
+	else if (getNumber == "c"){
 			cleanOperation();
-		} 
+		}
+	else if (getNumber == "="){
+		afficherResultat();
 	}
+}
 
 //Function to clean the result div when c is clicked
 function cleanOperation(){
 	$('#result').html('');
+}
+
+//Function to print the result on the calc
+function afficherResultat(){
+	$('#result').html(resultat);	
 }
