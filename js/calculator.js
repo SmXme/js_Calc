172ccb682cb1d6eb5f2ec06414192dfc36a222f2
SmXme <pillot.clement@gmail.com>
2017-11-28 09:52:44 +0100

    Created the functions and the event to make the calc buttons work

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,9 @@
 // add your code here to make the calculator work
+
+//		EventListeners		//
+$('.btn').click(addNumber);
+
+//		Functions		//
+function addNumber () {
+	$('#result').append($(this).html());
+}
